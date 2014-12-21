02ad0f0a6ca8423a8ca680af04267e3929c67eaf
Brent Russell <brussell@allsystemsgo.net>
2014-12-21 01:58:25 -0800

    get sale type from store pages and display on wishlist items

--- chrome-extension/js/wishlist-plugin.js
+++ chrome-extension/js/wishlist-plugin.js
@@ -1,3 +1,29 @@
-$(document).ready(function(){
-	alert('test');
+$(document).ready(function()
+{
+	var wishlist_items = $('#wishlist_items .wishlistRow');
+	
+	wishlist_items.each(function(i)
+	{
+		var $wishlist_item = $(this);
+		var $wishlist_row_item = $wishlist_item.find('.wishlistRowItem');
+		var $price_data = $wishlist_row_item.find('.gameListPriceData');
+		var store_page_url = $wishlist_item.find('.gameListPriceData .storepage_btn_ctn a').attr('href'); // get the "Visit Store Page" anchor
+
+		$price_data.children().first().before('<div class="sale-data"></div>');
+		
+		$.get(store_page_url, function(store_page_html)
+		{
+			// find first buy section & report sale type info
+			var $store_document = $($.parseHTML(store_page_html, null, true));
+			var $sales = $store_document.find('#game_area_purchase .game_area_purchase_game_wrapper .game_area_purchase_game .game_purchase_discount_countdown');
+			var $sale_data = $wishlist_row_item.find('.sale-data');
+			$sale_data.add().html($sales.first());
+			
+			//TODO:
+			// implement daily deal countdown timer
+			// find "Packages that include this game" section & report package sale info
+			// show all purchase variants
+			// add "add to cart" for each purchase option
+		}, 'html');
+	});
 });
