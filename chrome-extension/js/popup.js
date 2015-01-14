2e31419387be96dc621c43e31e439bc1fee71d1a
Brent Russell <brussell@allsystemsgo.net>
2015-01-14 00:44:06 -0800

    popuplate settings input form with values from local storage

--- chrome-extension/js/popup.js
+++ chrome-extension/js/popup.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
+	// populate input form with values from local storage
+	$('input:not(#save)').each(function(){
+		$(this).val(localStorage[this.id]);
+	});
+
 	$('#save').click(function(){
+		// save input data to local storage
 		$('input:not(#save)').each(function(){
 			localStorage[this.id] = $(this).val();
 		});
