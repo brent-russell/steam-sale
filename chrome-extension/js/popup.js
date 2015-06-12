9caa44c63f676c6cce56df840fc6710079f461b9
Brent Russell <brussell@allsystemsgo.net>
2015-06-11 20:06:57 -0700

    set and get extension settings to and from chrome storage

--- chrome-extension/js/popup.js
+++ chrome-extension/js/popup.js
@@ -1,13 +1,19 @@
 $(document).ready(function(){
 	// populate input form with values from local storage
-	$('input:not(#save)').each(function(){
-		$(this).val(localStorage[this.id]);
+	chrome.storage.local.get('steam-sale-helper', function(result) {
+		$('#country-code').val(result['steam-sale-helper']['country-code']);
+		$('#language').val(result['steam-sale-helper']['language']);
 	});
 
 	$('#save').click(function(){
 		// save input data to local storage
-		$('input:not(#save)').each(function(){
-			localStorage[this.id] = $(this).val();
+		chrome.storage.local.set(
+		{
+			'steam-sale-helper':
+			{
+				'country-code': $('#country-code').val(),
+				'language': $('#language').val()
+			}
 		});
 	});
 });
